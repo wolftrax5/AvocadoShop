0709b1ebe65a3f70186d1d3a7bf95323aa2ad5f0
Alejandro Medina <5almero5@gmail.com>
2022-12-20 20:47:50 -0600

    fetch & render data

--- src/index.js
+++ src/index.js
@@ -3,4 +3,33 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log("Happy hacking :)");
+const BASE_URL = "https://platzi-avo.vercel.app";
+const AVOCADO_URL = `${BASE_URL}/api/avo`;
+const APP_NODE = document.querySelector("#app");
+
+const generateNode = (element) => {
+  const image = document.createElement("img");
+  image.src = `${BASE_URL}${element.image}`;
+  const title = document.createElement("h3");
+  title.textContent = element.name;
+  const price = document.createElement("div");
+  price.textContent = element.price;
+  const container = document.createElement("section");
+  container.append(title, image, price);
+  return container;
+};
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(AVOCADO_URL);
+    const dataResponse = await response.json();
+    const avoNodeList = dataResponse.data.map(generateNode);
+
+    APP_NODE.append(...avoNodeList);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchData();
